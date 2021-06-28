eec4430f445d4187537b87831bb804c2ed4699e2
David Verghese <dj.verghese@gmail.com>
2021-06-28 16:27:41 -0400

    website redesign

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom";
 import './App.css';
 
 function Home() {
-  return <div className="home">
+  return <div><div className="home">
     
-   
-  
+    <h1>The Greatest Rock Songs of the 1960s</h1>
+    <p>... according to me</p>
+    <button id="home-button">Begin</button>
+  </div>
+{/*   
     <div className="home-pics">
       <img alt="George Harrison playing sitar" src="https://media4.giphy.com/media/mrZeRenrWh85O/200.webp?cid=ecf05e47okr8ddfy0qt8i6d1xto50vw8laqdgd2uxl8obkqn&rid=200.webp&ct=g"/>
       <img alt="Jimi Hendrix playing acoustic" src="https://media4.giphy.com/media/JNMFUwShabBUQ/giphy.gif?cid=ecf05e472mw7jfeic26xtpry3dep28wu8hpqt2rlzdg122wg&rid=giphy.gif&ct=g"/>
@@ -19,7 +22,7 @@ function Home() {
         <Link to="/playlist/0"> <button>Start playlist</button></Link>
       <img alt="Eric Claption's guitar" src="https://i0.wp.com/www.guitarhangar.com/wp-content/uploads/2018/12/999590_618123934899227_1319880217_n.jpg?fit=630%2C529&ssl=1"/>
     </div>
-       
+        */}
 
   
   
